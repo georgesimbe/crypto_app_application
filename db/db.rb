c29868545c26287b97894ab0efbef4d8e12b99db
George <Georgesimbe@icloud.com>
2022-09-19 09:41:47 +0930

    Repush heroku

--- db/db.rb
+++ db/db.rb
@@ -1,6 +1,6 @@
 
 def run_sql(sql, sql_params = [])
-    db = PG.connect(ENV['COIN_API'] ||dbname: 'crypto_bookmark_db')
+    db = PG.connect(ENV['COIN_API'] || dbname: 'crypto_bookmark_db')
     results = db.exec_params(sql, sql_params)
     db.close
     results
